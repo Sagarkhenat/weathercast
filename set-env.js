30b085d519b3587b25e18b774da748ce83838f04
unknown <sgkhenat94@gmail.com>
2026-02-10 14:44:16 +0530

    set-env file updated to handle firebase config variable on vercel

--- set-env.js
+++ set-env.js
@@ -17,13 +17,13 @@ export const environment = {
   apiKey: '${process.env.apiKey || "YOUR_API_KEY"}',
   weatherBaseUrl:'https://api.openweathermap.org/data/2.5',
   firebaseConfig: {
-    apiKey: "${process.env.firebaseConfig.apiKey || "YOUR_API_KEY"}",
-    authDomain: "${process.env.firebaseConfig.authDomain || "YOUR_AUTH_DOMAIN"}",
-    projectId: "${process.env.firebaseConfig.projectId || "YOUR_PROJECT_ID"}",
-    storageBucket: "${process.env.firebaseConfig.storageBucket || "YOUR_STORAGE_BUCKET"}",
-    messagingSenderId: "${process.env.firebaseConfig.messagingSenderId || "YOUR_MESSAGING_ID"}",
-    appId: "${process.env.firebaseConfig.appId || "YOUR_API_ID"}",
-    measurementId: "${process.env.firebaseConfig.measurementId || "YOUR_MEASUREMENT_ID"}"
+    apiKey: "${process.env.FIREBASE_API_KEY || "YOUR_API_KEY"}",
+    authDomain: "${process.env.FIREBASE_AUTH_DOMAIN || "YOUR_AUTH_DOMAIN"}",
+    projectId: "${process.env.FIREBASE_PROJECT_ID || "YOUR_PROJECT_ID"}",
+    storageBucket: "${process.env.FIREBASE_STORAGE_BUCKET || "YOUR_STORAGE_BUCKET"}",
+    messagingSenderId: "${process.env.FIREBASE_MESSAGING_SENDER_ID || "YOUR_MESSAGING_ID"}",
+    appId: "${process.env.FIREBASE_APP_ID || "YOUR_API_ID"}",
+    measurementId: "${process.env.FIREBASE_MEASUREMENT_ID || "YOUR_MEASUREMENT_ID"}"
   }
 };
 `;
