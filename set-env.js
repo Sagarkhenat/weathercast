84623aefcdc18bad490e35c25d62900840b12ae2
unknown <sgkhenat94@gmail.com>
2026-02-10 14:35:15 +0530

    set-env file updated to handle firebaseConfig variable

--- set-env.js
+++ set-env.js
@@ -15,7 +15,16 @@ const envConfigFile = `
 export const environment = {
   production: true,
   apiKey: '${process.env.apiKey || "YOUR_API_KEY"}',
-  weatherBaseUrl:'https://api.openweathermap.org/data/2.5'
+  weatherBaseUrl:'https://api.openweathermap.org/data/2.5',
+  firebaseConfig: {
+    apiKey: "${process.env.firebaseConfig.apiKey || "YOUR_API_KEY"}",
+    authDomain: "${process.env.firebaseConfig.authDomain || "YOUR_AUTH_DOMAIN"}",
+    projectId: "${process.env.firebaseConfig.projectId || "YOUR_PROJECT_ID"}",
+    storageBucket: "${process.env.firebaseConfig.storageBucket || "YOUR_STORAGE_BUCKET"}",
+    messagingSenderId: "${process.env.firebaseConfig.messagingSenderId || "YOUR_MESSAGING_ID"}",
+    appId: "${process.env.firebaseConfig.appId || "YOUR_API_ID"}",
+    measurementId: "${process.env.firebaseConfig.measurementId || "YOUR_MEASUREMENT_ID"}"
+  }
 };
 `;
 
